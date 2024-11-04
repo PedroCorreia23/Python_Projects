b953e674ba9403ca116535aff30495a9868359a4
zini23 <pedro.correia.piruzzo@gmail.com>
2024-11-04 03:28:49 +0000

    import os

--- File Organizer/script.py
+++ File Organizer/script.py
@@ -1,7 +1,7 @@
 import os
 import shutil
 
-main_dir = r'C:\Users\pedro\OneDrive\Ambiente de Trabalho\file_organizer'
+main_dir = input("Paste the directory path of the folder you want to organize: ")
 
 file_categories = {
     "Images": ['.png', '.jpg', '.jpeg'],
@@ -10,29 +10,30 @@ file_categories = {
     "Audio": ['.mp3', '.wav'],
 }
 
-destination_folder = os.path.join(main_dir, file_categories.items())
-
 # Ensure the main directory exists
 if os.path.exists(main_dir):
     
-    if not os.path.exists(destination_folder):
-        os.mkdir(destination_folder)
-        print(f"Directory '{file_categories}' created successfully.")
-    else:
-        print(f"Directory '{file_categories}' already exists.")
+    for category, extensions in file_categories.items():
 
-    # Iterate over files in the main directory
-    for filename in os.listdir(main_dir):
-        if filename.endswith(('.png', '.jpg')):
-            source_file = os.path.join(main_dir, filename)
-            try:
-                # Move the file to the destination folder
-                shutil.move(source_file, destination_folder)
-                print(f"File '{filename}' moved to '{destination_folder}' successfully.")
-            except PermissionError:
-                print(f"Permission denied: Unable to move '{filename}'.")
-            except Exception as e:
-                print(f"An error occurred while moving '{filename}': {e}")
+        destination_folder = os.path.join(main_dir, category)
+        if not os.path.exists(destination_folder):
+            os.mkdir(destination_folder)
+            print(f"Directory '{category}' created successfully.")
+        else:
+            print(f"Directory '{category}' already exists.")
+
+        # Iterate over files in the main directory
+        for filename in os.listdir(main_dir):
+            if filename.endswith(tuple(extensions)): 
+                source_file = os.path.join(main_dir, filename)
+                try:
+                    # Move the file to the destination folder
+                    shutil.move(source_file, destination_folder)
+                    print(f"File '{filename}' moved to '{destination_folder}' successfully.")
+                except PermissionError:
+                    print(f"Permission denied: Unable to move '{filename}'.")
+                except Exception as e:
+                    print(f"An error occurred while moving '{filename}': {e}")
 else:
     print(f"Directory '{dir}' does not exist. Please check the path.")
 
