63aec12f0e1cd3d292409443b6f45c5a801f961d
zini23 <pedro.correia.piruzzo@gmail.com>
2024-11-09 03:57:55 +0000

    Add country indicator and improve weather app UI

--- WeatherApp/main.py
+++ WeatherApp/main.py
@@ -2,32 +2,42 @@ import requests
 from datetime import datetime
 import tkinter as tk
 from tkinter import messagebox
+from PIL import Image, ImageTk
+import io
 
-API_KEY = "dbc0df74b85a74a40682a00cdc651c0e"
+API_KEY = ""
 
 def setup_window(): 
     
     root = tk.Tk()
     root.title("Weather App")
+    root.configure(bg="#a3c0e0")
     root.geometry("500x500")
 
-    label_city = tk.Label(root, text="Enter city: ")
-    label_city.pack(pady=5)
-    entry_city = tk.Entry(root) 
-    entry_city.pack(pady=5)
-
-    #Show results 
-    label_results = tk.Label(root, text="", font=("Arial", 12), justify="left")
-    label_results.pack(pady=20)
+    entry_city = tk.Entry(root, font = "Helvetica, 18", fg="#627099", bg="#eff2fa") 
+    entry_city.pack(pady=10)
 
     # Search button
-    button = tk.Button(root, text="Search", command=lambda: get_weather(entry_city, label_results), fg="black")
-    button.pack()
+    button = tk.Button(root, text="Search", command=lambda: get_weather(entry_city, label_results, label_icon, label_city_name), 
+                       bg="#ff8e4c", fg="white", font=("Helvetica", 12), justify="left", width=7, height=1)
+    button.pack(pady=10)
+
+    #Label to show the city name
+    label_city_name = tk.Label(root, text="", font=("Arial", 20, "bold"), bg="#a3c0e0", fg="#eff2fa")
+    label_city_name.pack(pady=5)
+
+    # Label to show the weather icon
+    label_icon = tk.Label(root, bg="#a3c0e0")
+    label_icon.pack(pady=5)
+
+    #Label to show details (temperature, description and date)
+    label_results = tk.Label(root, text="", font=("Arial", 17))
+    label_results.configure(bg="#a3c0e0", fg="#eff2fa")
+    label_results.pack(pady=5)
 
     return root
 
-
-def get_weather(entry_city, label_results):
+def get_weather(entry_city, label_results, label_icon, label_city_name):
 
     city = entry_city.get()
     url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units=metric'
@@ -40,21 +50,31 @@ def get_weather(entry_city, label_results):
 
             main = data['main']
             weather = data['weather'][0]
+            country = data['sys']['country']
 
             temperature = main['temp']
-            feels_like = main['feels_like']
-            humidity = main['humidity']
+            icon_code = weather['icon']  # Icon code for weather condition
             weather_description = weather['description']
 
             current_time = datetime.now().strftime('%d-%m-%Y %H:%M:%S')
 
-            result_text = (f"City: {city}\n"
-                           f"Temperature: {temperature}°C\n"
-                           f"Feels Like: {feels_like}°C\n"
-                           f"Humidity: {humidity}%\n"
-                           f"Description: {weather_description.capitalize()}\n\n"
-                           f"Data Retrieved at {current_time}")
-            label_results.config(text = result_text)
+            print_city_name = (f"{city.capitalize()}, {country}\n")
+            label_city_name.config(text = print_city_name)
+
+            # Fetch the icon image
+            icon_url = f"http://openweathermap.org/img/wn/{icon_code}@2x.png"
+            icon_response = requests.get(icon_url)
+            icon_data = icon_response.content
+            icon_image = Image.open(io.BytesIO(icon_data))
+            icon_photo = ImageTk.PhotoImage(icon_image)
+            label_icon.config(image=icon_photo)
+            label_icon.image = icon_photo 
+
+            details = (
+                        f"Temperature: {temperature}°C\n"
+                        f"Description: {weather_description.capitalize()}\n\n"
+                        f"Data Retrieved at {current_time}")
+            label_results.config(text=details)
 
         else:
             messagebox.showerror("Error", "City not found.") 
@@ -64,4 +84,3 @@ def get_weather(entry_city, label_results):
 
 root = setup_window()
 root.mainloop()
-
