65a19f328ac8de30bbcd42d365f8b0a051b3dfed
zini23 <pedro.correia.piruzzo@gmail.com>
2024-11-10 02:55:39 +0000

    Improved file selection handling and image display workflow

--- Image Filter App/ui.py
+++ Image Filter App/ui.py
@@ -7,20 +7,43 @@ import io
 from tkinter import filedialog
 import os
 
+root = tk.Tk()
+
 def setup():
 
-    root = tk.Tk()
     root.title("Image Filter App")
-    root.geometry("500x500")
+    root.geometry("700x700")
 
-    open_folder_button = tk.Button(root, text="Select Folder", command=open_folder)
-    open_folder_button.pack(pady=10)
+    open_folder_button = tk.Button(root, text="Select Image", command=select_image)
+    open_folder_button.pack(pady=20)
 
     root.mainloop()
 
-def open_folder():
+def select_image():
+ 
+    file = filedialog.askopenfile()  # Opens a dialog to select a image
+    if file:
+        image_path = file.name
+        print("Selected Folder:", image_path)
+        display_image(image_path)
 
-    folder_path = filedialog.askdirectory()  # Opens a dialog to select a folder
-    print("Selected Folder:", folder_path)
+def display_image(image_path):
+    
+    #canvas size
+    canvas_width = 450
+    canvas_height = 450
+    
+    #resize image to fit in the canvas
+    img = Image.open(image_path)
+    img = img.resize((canvas_width, canvas_height), Image.LANCZOS)
+
+    # Create a canvas & display the image
+    canvas = tk.Canvas(root, width=canvas_width, height=canvas_height)
+    canvas.pack()
+
+    tk_img = ImageTk.PhotoImage(img)
+    canvas.create_image(0, 0, anchor=tk.NW, image=tk_img)
+
+    canvas.image = tk_img
 
 setup()
